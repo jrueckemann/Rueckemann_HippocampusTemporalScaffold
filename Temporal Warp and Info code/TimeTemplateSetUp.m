c2d09e718dc25f5714f9845e806ac1a66df9bd92
jrueckemann <62038189+jrueckemann@users.noreply.github.com>
2026-02-24 23:04:14 -0800

    added neural data and skeleton script
    added data from neurons in Figure 1 rasters.  Also added an example script to demonstrate the syntax for running the sample data.

--- Temporal Warp and Info code/TimeTemplateSetUp.m
+++ Temporal Warp and Info code/TimeTemplateSetUp.m
@@ -10,6 +10,3 @@ timetemplate.bindur=0.25;
 timetemplate.favorknot=nan(size(timetemplate.duration));
 timetemplate.knot=repmat({[ 0 0 ]}, size(timetemplate.duration));
 timetemplate.EventBounds=repmat({[-2 2]},size(timetemplate.EventName));
-timetemplate.EventNameF=...
-    [{'TrialStart'} {'LastMajorTurn'} {'Reward'}];
-timetemplate.EventBoundsF=repmat({[-2 2]},size(timetemplate.EventNameF));
